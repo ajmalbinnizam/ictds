69563c577ed3ec8b41af6945e70018ea324f0fed
ajmalbinnizam <ajmalnizam369@gmail.com>
2020-12-08 14:19:44 +0530

    section 15 completed

--- section15 flask/website  using flask/web.py
+++ section15 flask/website  using flask/web.py
@@ -1,8 +1,19 @@
 from flask import Flask,render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
-
 def home():
-    return
+    return render_template('home.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/courses')
+def courses():
+    return render_template('courses.html')
+
+if __name__ == '__main__':
+    app.run()
+    
